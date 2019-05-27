d9fad1c23794be5ab98988756000f200eb06bdca
Ambler <allinlbj@gmail.com>
2019-05-28 00:02:00 +0800

    up

--- 01-入门/node-demo/01/express-demo/server.js
+++ 01-入门/node-demo/01/express-demo/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 9999
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/xxx', (req, res) => res.send('我是/xxx'))
 
+app.get('/', (req, res) => {
+  // 当前路径与相对路径拼接，变成合理的路径
+  console.log(__dirname)
+  let p = path.join(__dirname,'./index.html')
+  console.log(p)
+  res.sendFile(p);
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
