a3097ab80f8a0fc679e380852b8e8270d82b7a19
Ambler <allinlbj@gmail.com>
2019-06-04 00:58:19 +0800

    timer

--- 01-入门/node-demo/03/timers.js
+++ 01-入门/node-demo/03/timers.js
@@ -16,7 +16,7 @@ const timeoutScheduled = Date.now();
 setTimeout(() => {
   // 处理Event loop，来到第一个阶段大概需要15ms呀！此时已经有了callback，在timer队列里边了
   const delay = Date.now() - timeoutScheduled;
-
+  
   console.log(`${delay}毫秒后执行了 setTimeout 的回调`);
 }, 100);
 // 如果你设置为20,那么此时还没有timer队列里边还没有callback，于是继续往下走
@@ -26,7 +26,11 @@ setTimeout(() => {
 someAsyncOperation(() => {
   const startCallback = Date.now();
   // 测试该10ms闹钟是否先于100ms闹钟执行
-  setTimeout(()=>{console.log('我是第二个setTimeout callback')},10)
+  setTimeout(()=>{
+    console.log('我是第二个setTimeout callback')
+    while (Date.now() - startCallback < 60) {
+    }
+  },10)
   // 执行一个耗时 10 毫秒的同步操作
   while (Date.now() - startCallback < 20) {
     console.log('我是第一个耗时95ms')
